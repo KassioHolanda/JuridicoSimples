ab5137da214ccf79676b7883722afce1cea402c7
KassioHolanda <holandakassio@hotmail.com>
2018-05-31 21:20:53 -0300

    atualizando data postagem

--- core/models.py
+++ core/models.py
@@ -23,6 +23,7 @@ class News(models.Model):
     theme = models.ForeignKey('Theme', related_name='news_theme', max_length=255, null=True, on_delete=models.CASCADE)
     body = models.CharField('Corpo da Noticia', max_length=255, null=False)
     image = models.FileField(upload_to=get_file_path)
+    user_register = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
